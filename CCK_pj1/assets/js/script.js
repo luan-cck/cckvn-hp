553179ed08a2e42d3892fa7cbd9b802c3e087107
Kota-704 <kktt14590358@gmail.com>
2024-09-20 19:20:19 +0900

    second commit

--- CCK_pj1/assets/js/script.js
+++ CCK_pj1/assets/js/script.js
@@ -1,8 +1,34 @@
 /*--------------------
+title_animation
+--------------------*/
+document.addEventListener('DOMContentLoaded', () => {
+  const titles = document.querySelectorAll('.animatedTitle');
+
+  titles.forEach(title => {
+    const text = title.innerHTML;
+    title.innerHTML = '';
+
+    text.split('').forEach((char, index) => {
+      const span = document.createElement('span');
+      span.classList.add('letter');
+      span.innerHTML = char === ' ' ? '&nbsp;' : char;
+      title.appendChild(span);
+
+      setTimeout(() => {
+        span.style.opacity = 1;
+      }, index * 20);
+    });
+  });
+});
+
+
+
+/*--------------------
 scroll_animation
 --------------------*/
 document.addEventListener('DOMContentLoaded', () => {
-  const elements = document.querySelectorAll('.scroll_animation');
+  const toRight = document.querySelectorAll('.scroll_right');
+  const toLeft = document.querySelectorAll('.scroll_left')
 
   const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
@@ -12,7 +38,23 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }, { threshold: 0.1 });
 
-  elements.forEach(element => {
-    observer.observe(element);
+  toRight.forEach(item => {
+    observer.observe(item);
   })
+
+  toLeft.forEach(item => {
+    observer.observe(item);
+  })
+})
+
+/*--------------------
+count_characters
+--------------------*/
+const textarea = document.getElementById('message_input');
+const countDisplay = document.getElementById('count_characters');
+const maxCharacters = 600;
+
+textarea.addEventListener('input', () => {
+  const remainingCharacters = maxCharacters - textarea.value.length;
+  countDisplay.textContent = `Remaining Characters: ${remainingCharacters}`;
 })
